22dcee1bf740b4ac7cc928f3339e2df259bdb6e4
pirate-lofy <pirate.lofy@gmail.com>
2023-08-06 23:10:24 +0300

    fix the save images function

--- pix2pix/configs.py
+++ pix2pix/configs.py
@@ -1,7 +1,7 @@
 import torch 
 
 LR=2e-4
-epochs=30
+epochs=50
 l1_lambda=100
 device='cuda' if torch.cuda.is_available() else 'cpu'
 adam_betas=(0.5,0.999)
--- pix2pix/train.py
+++ pix2pix/train.py
@@ -15,6 +15,7 @@ import numpy as np
 import argparse
 import albumentations as A
 from time import time
+from torchvision.utils import save_image
 
 
 def train_disc(disc,data,fake,bce,disc_optim,d_scaler):
@@ -53,9 +54,10 @@ def validate_batch(data,gen,disc,bce,L1):
 
     with torch.cuda.amp.autocast():
         d_fake=disc(x,fake.detach())
-        fake_loss=bce(d_fake,torch.ones_like(d_fake))
-        l1_loss=L1(fake,y)*l1_lambda
-        disc_loss=fake_loss+l1_loss
+        d_real=disc(x,y)
+        fake_loss=bce(d_fake,torch.zeros_like(d_fake))
+        real_loss=bce(d_real,torch.ones_like(d_real))
+        disc_loss=(fake_loss+real_loss)/2
 
         d_fake=disc(x,fake.detach())
         fake_loss=bce(d_fake,torch.ones_like(d_fake))
@@ -65,16 +67,30 @@ def validate_batch(data,gen,disc,bce,L1):
     return disc_loss.item(),gen_loss.item()
 
 
-def save_sample(x,i):
-    if not os.path.exists(saving_path):
-        os.mkdir(saving_path)
-    transform=A.Compose([
-            A.Normalize([-1,-1,-1],[1/0.5,1/0.5,1/0.5])
-        ])
-    for j,xx in enumerate(x):
-        fake=xx.permute([1,2,0]).detach().cpu().numpy()
-        fake=transform(image=fake)['image']
-        cv.imwrite(os.path.join(saving_path,f'{i}-{j}.jpg'),fake)
+# def save_sample(x,i):
+#     if not os.path.exists(saving_path):
+#         os.mkdir(saving_path)
+#     transform=A.Compose([
+#             A.Normalize([-1,-1,-1],[1/0.5,1/0.5,1/0.5])
+#         ])
+#     for j,xx in enumerate(x):
+#         fake=xx.permute([1,2,0]).detach().cpu().numpy()
+#         fake=transform(image=fake)['image']
+#         cv.imwrite(os.path.join(saving_path,f'{i}-{j}.jpg'),fake)
+
+def save_some_examples(gen, val_loader, epoch, folder=saving_path):
+    x, y = next(iter(val_loader))
+    x, y = x.to(device), y.to(device)
+    gen.eval()
+    with torch.no_grad():
+        y_fake = gen(x)
+        y_fake = y_fake * 0.5 + 0.5  # remove normalization#
+        z=torch.vstack([x * 0.5 + 0.5,y * 0.5 + 0.5,y_fake])
+        save_image(z, folder + f"/res_{epoch}.png")
+        # save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}.png")
+        if epoch == 1:
+            save_image(y * 0.5 + 0.5, folder + f"/label_{epoch}.png")
+    gen.train()
 
 
 def main(args):
@@ -97,13 +113,13 @@ def main(args):
 
     print(f'loaded optims and scalers in {time()-st}')
 
-    for epoch in trange(epochs):
+    for epoch in range(epochs):
         vg_loss,vd_loss,tg_loss,td_loss=[],[],[],[]
 
         gen.train()
         disc.train()
+        st=time()
         for ix,data in enumerate(tr_loader):
-            st=time()
             x,y=data
             fake=gen(x)
             disc_loss=train_disc(disc,data,fake,bce,disc_optim,disc_scaler)
@@ -111,8 +127,9 @@ def main(args):
             td_loss.append(disc_loss)
             tg_loss.append(gen_loss)
         
-        print(f'single training iteration tool {time()-st}')
-        writer.add_scalar('training generator loss',np.mean(td_loss),epoch)
+        # print(f'single training iteration took {time()-st}')
+
+        writer.add_scalar('training generator loss',np.mean(tg_loss),epoch)
         writer.add_scalar('training disc loss',np.mean(td_loss),epoch)
             
         gen.eval()
@@ -123,9 +140,11 @@ def main(args):
             vd_loss.append(disc_loss)
             vg_loss.append(gen_loss)
 
-        print(f'single validation iteration tool {time()-st}')
-        writer.add_scalar('validation generator loss',np.mean(td_loss),epoch)
-        writer.add_scalar('calidation disc loss',np.mean(td_loss),epoch)
+        # print(f'single validation iteration took {time()-st}')
+        writer.add_scalar('validation generator loss',np.mean(vg_loss),epoch)
+        writer.add_scalar('calidation disc loss',np.mean(vd_loss),epoch)
+
+        print(epoch ,round(np.mean(tg_loss),4),round(np.mean(td_loss),4),round(np.mean(vg_loss),4),round(np.mean(vd_loss),4))
 
         if epoch%5==0:
             with torch.no_grad():
@@ -136,7 +155,8 @@ def main(args):
                 writer.add_image('real',real,epoch)
                 writer.add_image('fake',fake,epoch)
 
-                save_sample(fake_data,epoch)
+                # save_sample(fake_data,epoch)
+                save_some_examples(gen, val_loader, epoch, saving_path)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description="A simple script that greets the user.")
